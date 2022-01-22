08872cf94d920c822a639a0fded5f2eeb2165c16
mezeon <mezeon@int.pl>
2022-01-22 17:42:41 +0100

    v1.1.0: faster generation

--- README.txt
+++ README.txt
@@ -1,10 +1,10 @@
 Author: Mezeon (Jakub) <mezeon@int.pl>
 
-Version: 1.0.0
-Date: 16-01-2022
+Version: 1.1.0
+Date: 22-01-2022
 Usage: python maze-generator.py [size] [offshoots]
-size - size of the maze, output image will have size (size, size + 1)
-offshoots - number of offshoots, which the maze will have
-The default values are size = 75 and offshoots = 300.
+size - size of the maze, output image will have size (size, size)
+offshoots - level of offshoots, which the maze will have
+The default values are size = offshoots = 100.
 
 Note: Generation of big mazes may take a lot of time.
--- src/maze-generator.py
+++ src/maze-generator.py
@@ -1,67 +1,76 @@
 from random import randint as r
 from sys import argv
 from PIL import Image
+from time import time
 
+start = time()
 blocks = set()
 
+# size of the maze - output image will have size (size, size)
 try: size = int(argv[1])
-except: size = 75
+except: size = 100
 
+# level of offshoots
 try: offshoots = int(argv[2])
-except: offshoots = 300
+except: offshoots = 100
 
-img = Image.new('1', (size + 2, size + 2))
+maze = []
+for w in range(size + 2):
+    maze.append([])
+    for h in range(size + 2):
+        maze[w].append(0)
 
-img.putpixel((size // 2, 0), 1)
-img.putpixel((size // 2, 1), 1)
+maze[size // 2][0] = 1
+maze[size // 2][1] = 1
 
-# main line
 last_step = [size // 2, 1]
 for i in range(offshoots):
     last_step = (r(0, size - 1), r(0, size - 1))
-    while img.getpixel(last_step) != 1:
+    while maze[last_step[0]][last_step[1]] != 1:
         last_step = (r(0, size - 1), r(0, size - 1))
-    while last_step[1] != size:
+    while last_step[1] != size - 2:
         # 0 - left, 1 - top, 2 - right, 3 - bottom
-        # coordinates are starting from upper left corner
+        # coordinates are starting from the upper left corner
         direction = r(0, 3)
-
-        if (direction == 0) and (img.getpixel((last_step[0] - 1, last_step[1] - 1)) == 0) and (img.getpixel((last_step[0] - 2, last_step[1] - 1)) == 0) and (img.getpixel((last_step[0] - 2, last_step[1])) == 0) and (img.getpixel((last_step[0] - 2, last_step[1] + 1)) == 0) and (img.getpixel((last_step[0] - 1, last_step[1] + 1)) == 0) and (last_step[0] - 1 > 0):
-                last_step = (last_step[0] - 1, last_step[1])
-                img.putpixel(last_step, 1)
-                blocks = set()
-
-        elif (direction == 1) and (img.getpixel((last_step[0] - 1, last_step[1] - 1)) == 0) and (img.getpixel((last_step[0] - 1, last_step[1] - 2)) == 0) and (img.getpixel((last_step[0], last_step[1] - 2)) == 0) and (img.getpixel((last_step[0] + 1, last_step[1] - 2)) == 0) and (img.getpixel((last_step[0] + 1, last_step[1] - 1)) == 0) and (last_step[1] - 1 > 0):
-            last_step = (last_step[0], last_step[1] - 1)
-            img.putpixel(last_step, 1)
+    
+        if (direction == 0) and (maze[last_step[0] - 1][last_step[1] - 1] == 0) and (maze[last_step[0] - 2][last_step[1] - 1] == 0) and (maze[last_step[0] - 2][last_step[1]] == 0) and (maze[last_step[0] - 2][last_step[1] + 1] == 0) and (maze[last_step[0] - 1][last_step[1] + 1] == 0) and (last_step[0] - 1 > 0):
+            last_step = (last_step[0] - 1, last_step[1])
+            maze[last_step[0]][last_step[1]] = 1
             blocks = set()
 
-        elif (direction == 2) and (img.getpixel((last_step[0] + 1, last_step[1] - 1)) == 0) and (img.getpixel((last_step[0] + 2, last_step[1] - 1)) == 0) and (img.getpixel((last_step[0] + 2, last_step[1])) == 0) and (img.getpixel((last_step[0] + 2, last_step[1] + 1)) == 0) and (img.getpixel((last_step[0] + 1, last_step[1] + 1)) == 0) and (last_step[0] + 1 < size - 1):
-                last_step = (last_step[0] + 1, last_step[1])
-                img.putpixel(last_step, 1)
-                blocks = set()
+        elif (direction == 1) and (maze[last_step[0] - 1][last_step[1] - 1] == 0) and (maze[last_step[0] - 1][last_step[1] - 2] == 0) and (maze[last_step[0]][last_step[1] - 2] == 0) and (maze[last_step[0] + 1][last_step[1] - 2] == 0) and (maze[last_step[0] + 1][last_step[1] - 1] == 0) and (last_step[1] - 1 > 0):
+            last_step = (last_step[0], last_step[1] - 1)
+            maze[last_step[0]][last_step[1]] = 1
+            blocks = set()
 
-        elif (direction == 3) and (img.getpixel((last_step[0] - 1, last_step[1] + 1)) == 0) and (img.getpixel((last_step[0] - 1, last_step[1] + 2)) == 0) and (img.getpixel((last_step[0], last_step[1] + 2)) == 0) and (img.getpixel((last_step[0] + 1, last_step[1] + 2)) == 0) and (img.getpixel((last_step[0] + 1, last_step[1] + 1)) == 0):
-                last_step = (last_step[0], last_step[1] + 1)
-                img.putpixel(last_step, 1)
-                blocks = set()
+        elif (direction == 2) and (maze[last_step[0] + 1][last_step[1] - 1] == 0) and (maze[last_step[0] + 2][last_step[1] - 1] == 0) and (maze[last_step[0] + 2][last_step[1]] == 0) and (maze[last_step[0] + 2][last_step[1] + 1] == 0) and (maze[last_step[0] + 1][last_step[1] + 1] == 0) and (last_step[0] + 1 < size - 1):
+            last_step = (last_step[0] + 1, last_step[1])
+            maze[last_step[0]][last_step[1]] = 1
+            blocks = set()
+
+        elif (direction == 3) and (maze[last_step[0] - 1][last_step[1] + 1] == 0) and (maze[last_step[0] - 1][last_step[1] + 2] == 0) and (maze[last_step[0]][last_step[1] + 2] == 0) and (maze[last_step[0] + 1][last_step[1] + 2] == 0) and (maze[last_step[0] + 1][last_step[1] + 1] == 0) and (last_step[1] + 1 < size - 1):
+            last_step = (last_step[0], last_step[1] + 1)
+            maze[last_step[0]][last_step[1]] = 1
+            blocks = set()
 
         else:
             blocks.add(direction)
         
         if len(blocks) == 4:
             last_step = (r(0, size - 1), r(0, size - 1))
-            while img.getpixel(last_step) != 1:
+            while maze[last_step[0]][last_step[1]] != 1:
                 last_step = (r(0, size - 1), r(0, size - 1))
-    
-img = img.crop((0, 0, size, size + 1))
-for i in range(size):
-    img.putpixel((i, size), 0)
 
-exit_coordinates = (r(0, img.size[0] - 1), img.size[1] - 2)
-while img.getpixel(exit_coordinates) != 1:
-    exit_coordinates = (r(0, img.size[0] - 1), img.size[1] - 2)
-img.putpixel((exit_coordinates[0], exit_coordinates[1] + 1), 1)
+exit_coordinates = (r(0, size - 1), size - 2)
+while maze[exit_coordinates[0]][exit_coordinates[1]] != 1:
+    exit_coordinates = (r(0, size - 1), size - 2)
+maze[exit_coordinates[0]][exit_coordinates[1] + 1] = 1
+
+img = Image.new('1', (size, size))
+for w in range(size):
+    for h in range(size):
+        img.putpixel((w, h), maze[w][h])
 
 img.save('maze.png')
 print('Maze saved as maze.png\a')
+print('Generation took', round(time() - start, 3), 's')
